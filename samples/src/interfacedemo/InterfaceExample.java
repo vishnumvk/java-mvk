185b2595db8aec19ad3c9e7e05f94bb9faa62ba5
mvish <mvish@172.22.125.95>
2022-04-13 19:33:14 +0530

    add interface demo

---
+++ samples/src/interfacedemo/InterfaceExample.java
@@ -0,0 +1,25 @@
+package interfacedemo;
+interface MyInterface{
+	   public static int num = 100;
+	   public default void display() {
+	      System.out.println("display method of MyInterface");
+	   }
+	    void print();
+	}
+public class InterfaceExample implements MyInterface {
+	 public static void main(String args[]) {
+	      InterfaceExample obj = new InterfaceExample();
+
+
+	      obj.display();
+	     // obj.num=30; cannot be changed since interface members are final
+	      System.out.println(MyInterface.num);
+	      obj.print();
+	   }
+
+	@Override
+	public void print() {
+		System.out.println("Overriden print");
+		
+	}
+}
---
+++ samples/src/interfacedemo/InterfaceExample2.java
@@ -0,0 +1,45 @@
+package interfacedemo;
+interface MyInterface1{
+	   public static int num = 100;
+	   public default void display() {
+	      System.out.println("display method of MyInterface1");
+	   }
+	    void print();
+	}
+interface MyInterface2{
+	   public static int num = 100;
+	   public default void display() {
+	      System.out.println("display method of MyInterface2");
+	   }
+	    void print();
+	    void show();
+	}
+public class InterfaceExample2 implements MyInterface1,MyInterface2 {
+
+	public static void main(String[] args) {
+		InterfaceExample2 it = new InterfaceExample2();
+          it.display();
+          it.print();
+          it.show();
+	}
+
+	@Override
+	public void display() {
+		
+		MyInterface1.super.display();
+	}
+
+	@Override
+	public void print() {
+		System.out.println("Overriden print");
+		
+	}
+
+	@Override
+	public void show() {
+		System.out.println("Overriden show");
+		
+		
+	}
+
+}
