fc620482f92f07432187f2caf558df1241c020d5
mvish <mvish@172.22.125.95>
2022-04-19 14:44:43 +0530

    oops

---
+++ samples/src/oops_examples/inheritance/SimpleInheritance.java
@@ -0,0 +1,41 @@
+package oops_examples.inheritance;
+
+
+//Dog, Cow (IS-A) Animal
+//Dog , Cow inherits eat method and legs from Animal
+class Animal{
+	int legs=4;
+	public void makeSound() {
+		System.out.println("ufff .... uff..");
+	}
+	public void eat() {
+		System.out.println("Eating ...");
+	}
+}
+
+class Dog extends Animal{
+	// Dog inherits features of Animal
+	String Name;
+	public void makeSound() {
+		System.out.println("Bow ..... bow..");
+	}
+}
+class Cow extends Animal{
+	// Cow inherits features of Animal
+	String Name;
+	public void makeSound() {
+		System.out.println("Mooo ..... Mooo..");
+	}
+}
+public class SimpleInheritance {
+
+	public static void main(String[] args) {
+		Dog d1 = new Dog();
+		Cow c1 = new Cow();
+		d1.makeSound();
+		d1.eat();
+		c1.makeSound();
+		c1.eat();
+	}
+
+}
