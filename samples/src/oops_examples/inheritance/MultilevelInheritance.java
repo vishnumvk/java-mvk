de9c489c0876f80bb1cc5dae59446f8ecc5de6d3
mvish <mvish@172.22.125.95>
2022-04-20 12:06:28 +0530

    new

--- samples/src/oops_examples/inheritance/MultilevelInheritance.java
+++ samples/src/oops_examples/inheritance/MultilevelInheritance.java
@@ -1,18 +1,28 @@
 package oops_examples.inheritance;
 
-class Animal1{  
-void eat(){System.out.println("eating...");}  
-}  
-class Dog1 extends Animal1{  
-void bark(){System.out.println("barking...");}  
-}  
-class BabyDog extends Dog1{  
-void weep(){System.out.println("weeping...");}  
-}  
-class MultilevelInheritance{  
-public static void main(String args[]){  
-BabyDog d=new BabyDog();  
-d.weep();  
-d.bark();  
-d.eat();  
-}}  
+class Animal_ {
+	void eat() {
+		System.out.println("eating...");
+	}
+}
+
+class Dog_ extends Animal_ {
+	void bark() {
+		System.out.println("barking...");
+	}
+}
+
+class BabyDog extends Dog_ {
+	void weep() {
+		System.out.println("weeping...");
+	}
+}
+
+class MultilevelInheritance {
+	public static void main(String args[]) {
+		BabyDog d = new BabyDog();
+		d.weep();
+		d.bark();
+		d.eat();
+	}
+}
